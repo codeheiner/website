8e60193cc037f43642ff73959d0aa187c175f835
codeheiner <info@codeheiner.de>
2023-04-01 22:48:46 +0200

    ..

--- src/lib/Kursangebot.js
+++ src/lib/Kursangebot.js
@@ -1,16 +1,56 @@
 export let kurse = [
     {
         "price": "price_1MnOIbDlv47dRGNIEoIojwrw", 
-        "name": "Programmieren für Einsteiger", 
-        "beschreibung": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium."},
+        "name": "Programmieren für Einsteiger",
+        "rows": [        
+            ["Datentypen", "Klassen", "Nebenläufigkeit"],
+            ["Funktionen", "Fehlersuche", "Softskills"],
+            ["Schleifen", "Logging", "Projekt"],
+        ],
+        "beschreibung": "\
+        In diesem Kurs werden Sie zum Programmierer!\
+        Klar, in einem dreitägigen Kurs kann man nicht zum Profi werden,\
+        aber wir geben unser Bestes um Ihnen den leichtesten Start zu ermöglichen.\
+        Nach Abschluss des Kurses werden Sie prinzipiell in der Lage sein beliebige\
+        Programme zu schreiben, zu ermitteln was eine sinnvolle und effiziente Architektur \
+        für Ihr Projekt ist und was guter Code leistet.\
+        "},
     
     {
         "price": "price_1MqwM4Dlv47dRGNIHsJ3ANhk", 
-        "name": "Grundlagen des Maschinellen Lernens", 
-        "beschreibung": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium."},
+        "name": "Grundlagen des Maschinellen Lernens",
+        "rows": [        
+            ["Was ist ML?", "Regression", "Neuronale Netze und Deep Learning"],
+            ["Mathematische Grundlagen", "Naive Bayes", "Reinforcement Learning"],
+            ["relevante Python Bibliotheken", "Support Vector Machines", "Projekt"],
+        ],
+        "beschreibung": "\
+        Den Kurs 'Grundlagen des Maschinellen Lernens' beginnen wir mit einem Überblick\
+        über die verschiedenen Bereiche dieses spannenden Feldes und motivieren die \
+        verschiedenen Ansätze des Maschinellen Lernens anhand anschaulicher Beispiele.\
+        Dann steigen wir mit den wichtigsten mathematischen Grundlagen in die Theorie ein, \
+        lernen die wichtigsten Python-Bibliotheken kennen und bearbeiten danach gemeinsam\
+        einige Miniprojekte aus jeweils einem Teilbereich des ML. Der Kurs schließt \
+        mit einem Projekt ab, welches in Partnerarbeit bearbeitet wird.\
+        "},
     
     {
         "price": "price_1MrlqeDlv47dRGNI8ZzF0MOC", 
         "name": "Einführung in Data Science mit Python", 
-        "beschreibung": "orem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium."}
+        "rows": [        
+            ["Was ist Data Science?", "Daten sammeln und bereinigen", "Maschinelles Lernen"],
+            ["Mathematische Grundlagen", "Datenbanken / Datalakes", "Projekt"],
+            ["Python Grundlagen", "Visualisierung", "Evaluation"],
+        ],
+        "beschreibung": "\
+        Dieser Kurs ist Ihre Einstieg in Data Science!\
+        Zu Beginn des Kurses werden wir herausarbeiten, was Data Science eigentlich ist und mithin zu verstehen,\
+        was Data Science von anderen Bereichen abgrenzt. Im weiteren werden wir die mathematischen \
+        Grundlagen aufarbeiten die ein Data Scientist beherrschen sollte und für diese eine gewisse Intuition aufbauen.\
+        Außerdem werden wir in versch. Bereiche des Maschinelle Lernens schnuppern, da maschinelles Lernen und \
+        Data Science in einem gewissen Maße in einander übergehen. Schlussendlich werden wir unser neu gewonnenes\
+        Wissen nutzen, um von höherer Warte aus einen Überblick über die bestehende Landschaft von vorgefertigten Lösungen\
+        und Tools zu bekommen, die Data Scientists im Alltag nutzen, um sich das Leben leichter zu machen. Speziell\
+        diese letzte Station ist wichtig, da die Fülle an Tools auf dem Markt gigantische Ausmaße angenommen hat.\
+        "}
 ]
--- src/routes/kurse/[id]/+page.svelte
+++ src/routes/kurse/[id]/+page.svelte
@@ -13,49 +13,31 @@
           <!-- <li>{ $page.params.id }</li> -->
         </ul>
     </nav>
-    <h2>{ data.name }</h2>
+    <h1>{ data.name }</h1>
     <!-- <a style={"background-color: #546e7a; border:none"} role="button" href=".">Kursplan (PDF)</a> -->
     <p>{ data.beschreibung }</p>
 
 
-    <h3>Kursübersicht</h3>
-    <p>
-      Sinnvolle Gliederung und ausreichende Pausen sind wichtig. 
-      Darum trifft sich der Kurs nach Absprache an drei oder an vier Tagen.
-    </p>
+    <h2>Kursübersicht</h2>
 
   <table>
     <thead>
       <tr>
         <th scope="col">Modul</th>
-        <th scope="col">Do</th>
-        <th scope="col">Fr</th>
-        <th scope="col">Sa</th>
-        <th scope="col">So</th>
+        <th scope="col">Freitag</th>
+        <th scope="col">Samstag</th>
+        <th scope="col">Sonntag</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Datentypen</td>
-        <td>Klassen</td>
-        <td>Nebenläufigkeit</td>
-        <td>Handwerkszeug</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Funktionen</td>
-        <td>Errors</td>
-        <td>Effizientes Python</td>
-        <td>Softskills</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Schleifen</td>
-        <td>Fehlersuche</td>
-        <td>Asyncio</td>
-        <td>Projekt</td>
-      </tr>
+        {#each data.rows as rows, i}
+            <tr>
+                <td>{ i +1 }</td>
+                {#each rows as row}
+                    <td>{ row }</td>
+                {/each}
+            </tr>
+        {/each}
     </tbody>
     <!-- <tfoot>
       <tr>
