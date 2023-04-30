78ab228f6f383226fb62454456e351a4cfff8d0c
codeheiner <info@codeheiner.de>
2023-04-30 09:06:40 +0200

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,7 +51,7 @@
         <h2>Die KIs sind Ihnen auf den Fersen. Lernen Sie heute worauf es morgen ankommt.<br>
       </h2>
       </hgroup>
-      <p><a href="/Kursübersicht.pdf" role="button" class="primary" download>Kursangebot als PDF downloaden</a></p>
+      <p><a href="kursubersicht.pdf" role="button" class="primary" download>Kursangebot als PDF downloaden</a></p>
     </header>
 
   </div>
